05686f795f40246f4d99682aa2b489616639b87d
Ducdong94 <ducdong94@gmail.com>
2018-05-16 01:25:48 +0400

    CRUD-demo

---
+++ CRUD-demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUD_demo.MvcApplication" Language="C#" %>
---
+++ CRUD-demo/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_demo.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string Desc { get; set; }
+    }
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
